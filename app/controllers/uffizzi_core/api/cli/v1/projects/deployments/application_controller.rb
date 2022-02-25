80375af9aae689dfc159ebe4e720d0a6abc16b75
moklidia <kitsunegakkou@gmail.com>
2022-02-25 13:04:28 +0300

    wip

---
+++ app/controllers/uffizzi_core/api/cli/v1/projects/deployments/application_controller.rb
@@ -0,0 +1,8 @@
+
+# frozen_string_literal: true
+
+class UffizziCore::Api::Cli::V1::Projects::Deployments::ApplicationController < UffizziCore::Api::Cli::V1::Projects::ApplicationController
+  def resource_deployment
+    @resource_deployment ||= resource_project.deployments.find(params[:deployment_id])
+  end
+end
